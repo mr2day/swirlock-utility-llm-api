998f7bd650f4f37fb7a660c8dc68dd7ccdc4ba96
Nick Beres <beres.nick@gmail.com>
2026-04-30 00:33:23 +0300

    Set PM2 model to gemma

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
         PORT: 3000,
         OLLAMA_HOST: 'http://127.0.0.1:11434',
         OLLAMA_MODELS: 'qwen3.5:9b,gemma4:e4b',
-        OLLAMA_MODEL: 'qwen3.5:9b',
+        OLLAMA_MODEL: 'gemma4:e4b',
         OLLAMA_KEEP_ALIVE: '-1',
         PRELOAD_MODEL: 'true',
         MODEL_IMAGE_INPUT: 'true',
